2277d38e6dc01a53372f995a245f5ea5769eda8b
Ardi Ismail <>
2019-10-01 11:32:12 +0700

    add perimeter and area of rectangle

--- shapes.go
+++ shapes.go
@@ -2,5 +2,10 @@ package smi
 
 // Perimeter will calculate the rectangle
 func Perimeter(width, height float64) float64 {
-	return 0
+	return 2 * (width + height)
+}
+
+// Area will calculate area of rectangle
+func Area(width, height float64) float64 {
+	return width * height
 }
