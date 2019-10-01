7a703d6d9f09ddb8a4dcc6a2f4ce5bd661a6c8cf
Ardi Ismail <>
2019-10-01 14:37:38 +0700

    add additional UT

--- shapes.go
+++ shapes.go
@@ -1,17 +1,29 @@
 package smi
 
+import "math"
+
 // Rectangle has a dimension of a rectangle
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+// Area returns the area of the rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle has only radius
+type Circle struct {
+	Radius float64
+}
+
+// Area returns the area of Circles
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 // Perimeter will calculate the rectangle
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
-}
-
-// Area will calculate area of rectangle
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
 }
